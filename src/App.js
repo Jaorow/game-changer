3b53649d52cff7a843bd1968fd1478a63794b49c
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-08-05 13:29:52 +1200

    --making a start--

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ function App() {
     <div className="App">
       
       <h1 id = "header" >GAME CHANGER</h1>
+      <a href="highscores.html"></a>
+      <div id = "gamezone">
+        game goes here
+        
 
+        <p>TODO</p> 
+        <p> - add second page which shows high scores</p> 
+
+        <p>- find some way to record results in simple text file</p> 
+        <p> - make input for top scores</p> 
+        <p>- make countdown (could be bar) till next prize</p>
+        <p> - make very basic game to test </p> 
+        <p>SPREAD THE WORD!</p>
+
+        
+      </div>
       
       
       
